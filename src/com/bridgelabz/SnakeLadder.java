c9dbcce2d15c478fbb943c7b9e94e8643776e7ab
Samim Aktar <samimaktar0773@gmail.com>
2021-10-08 15:57:05 +0530

    Done UC3 player choose option

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -4,12 +4,38 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("Lets play snake ladder Game");
 		
+		// start position is zero
 		int startPosition = 0;
+		
+		System.out.println("start position: " + startPosition);
+		
 		//Random Dice Value btn 1 to 6
 		int diceValue=(int) Math.floor((Math.random()*6) + 1);
+		
 		//check the dice value
 		System.out.println(diceValue);
 		
+		//random number for no play ladder and snake
+		int checkOption = (int)(Math.random() * 10) % 3 + 1;
+		
+		//check the random value
+		System.out.println("check "+checkOption);
+		
+		switch(checkOption) {
+			// case for ladder
+			case 1:
+				startPosition = startPosition + diceValue;
+				break;
+			// case for snake 
+			case 2:
+				startPosition = startPosition - diceValue;
+				break;
+			// case for not playing
+			default:
+				System.out.println("Player is not playing " + startPosition);
+		}
+		// check position after player choode a option 
+		System.out.println("position after  player choose a option : "+startPosition);
 	}
 	
 	
