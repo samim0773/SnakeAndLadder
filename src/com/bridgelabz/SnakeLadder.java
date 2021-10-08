10326ddfa43a3a6a44cb4f0b6289a68bf0923938
Samim Aktar <samimaktar0773@gmail.com>
2021-10-08 15:30:48 +0530

    Done UC2- get the number btn 1 to 6

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -5,6 +5,10 @@ public class SnakeLadder {
 		System.out.println("Lets play snake ladder Game");
 		
 		int startPosition = 0;
+		//Random Dice Value btn 1 to 6
+		int diceValue=(int) Math.floor((Math.random()*6) + 1);
+		//check the dice value
+		System.out.println(diceValue);
 		
 	}
 	
