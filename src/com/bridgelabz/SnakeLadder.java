2a1a9ca2e59a6ff518fc434ab7a809c86ca8cf56
Samim Aktar <samimaktar0773@gmail.com>
2021-10-08 15:23:24 +0530

    Done UC1- Single player start position 0

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -3,6 +3,11 @@ package com.bridgelabz;
 public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("Lets play snake ladder Game");
+		
+		int startPosition = 0;
+		
 	}
+	
+	
 
 }
