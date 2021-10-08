3321be9bce6854b33802fe30330d6d1c2a84efcc
Samim Aktar <samimaktar0773@gmail.com>
2021-10-08 21:37:14 +0530

    Done UC5 Winning position 100

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -15,30 +15,37 @@ public class SnakeLadder {
 		//check the dice value
 		System.out.println("diceValue: "+diceValue);
 		
-		//random number for no play ladder and snake
-		int checkOption = (int)(Math.random() * 10) % 3 + 1;
-		
-		//check the random value
-		System.out.println("check "+checkOption);
-		
-		switch(checkOption) {
-			// case for ladder
-			case 1:
-				startPosition = startPosition + diceValue;
-				break;
-			// case for snake 
-			case 2:
-				startPosition = startPosition - diceValue;
-				if(startPosition < 0) {
-					startPosition = 0;
-				}
-				break;
-			// case for not playing
-			default:
-				System.out.println("Player is not playing " + startPosition);
+		// winning condition check
+		while(startPosition < 100) {
+			
+			//random number for no play ladder and snake
+			int checkOption = (int)(Math.random() * 10) % 3 + 1;
+			
+			//check the random value
+			System.out.println("check "+checkOption);
+			
+			switch(checkOption) {
+				// case for ladder
+				case 1:
+					startPosition = startPosition + diceValue;
+					if(startPosition > 100) {
+						startPosition = startPosition -diceValue;
+					}
+					break;
+				// case for snake 
+				case 2:
+					startPosition = startPosition - diceValue;
+					if(startPosition < 0) {
+						startPosition = 0;
+					}
+					break;
+				// case for not playing
+				default:
+					System.out.println("Player is not playing " + startPosition);
+			}
+			// check position after player choode a option 
+			System.out.println("position after  player choose a option : "+startPosition);
 		}
-		// check position after player choode a option 
-		System.out.println("position after  player choose a option : "+startPosition);
 	}
 	
 	
